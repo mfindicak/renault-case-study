0200c626b4a1327a4bf947bcea227c0768cb20d3
mertfindicak <mfindicak@gmail.com>
2022-07-10 23:57:34 +0300

    Login changed to cookie based.

--- nodejs-backend/app.js
+++ nodejs-backend/app.js
@@ -1,13 +1,16 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const userRoutes = require('./src/users/routes');
 const authController = require('./src/auth/controller');
 
 const app = express();
 const port = 3000;
 
-app.use(cors({ origin: 'http://localhost:4200' })); //This is need for get request from frontend. Angular uses 4200 port. That library solves CORS issue.
+app.use(cors({ credentials: true, origin: 'http://localhost:4200' })); //This is need for get request from frontend. Angular uses 4200 port. That library solves CORS issue.
+
+app.use(cookieParser()); //We use this for read cookies.
 
 app.use(express.json());
 
