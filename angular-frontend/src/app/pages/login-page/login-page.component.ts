f9348ac8d6c0de2e462fd3942953670c343af7cd
mertfindicak <mfindicak@gmail.com>
2022-07-11 03:56:21 +0300

    Login errors are added to loginPage.

--- angular-frontend/src/app/pages/login-page/login-page.component.ts
+++ angular-frontend/src/app/pages/login-page/login-page.component.ts
@@ -8,6 +8,9 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./login-page.component.css'],
 })
 export class LoginPageComponent implements OnInit {
+  incorrectPassword: boolean = false;
+  couldNotFindUser: boolean = false;
+
   loginForm = new FormGroup({
     username: new FormControl('', [Validators.required]),
     password: new FormControl('', [Validators.required]),
@@ -26,11 +29,16 @@ export class LoginPageComponent implements OnInit {
   ngOnInit(): void {}
 
   onSubmit(): void {
+    this.incorrectPassword = false;
+    this.couldNotFindUser = false;
     this.authService.login(this.getUsername(), this.getPassword()).subscribe({
       next: (result) => {
         console.log(result);
       },
-      error: (e) => console.error(e),
+      error: (e) => {
+        if (e.status === 400) this.couldNotFindUser = true;
+        else if (e.status === 401) this.incorrectPassword = true;
+      },
       complete: () => {},
     });
   }
